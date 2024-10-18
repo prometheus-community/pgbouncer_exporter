b932e8a4c725736845f35c3bba43835f6329cc2d
Felix Yuan <felix.yuan@reddit.com>
2024-10-18 13:30:33 -0700

    Add collector test
    Signed-off-by: Felix Yuan <felix.yuan@reddit.com>

---
+++ collector_test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"testing"
+
+	"log/slog"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/smartystreets/goconvey/convey"
+)
+
+type labelMap map[string]string
+
+type MetricResult struct {
+	labels     labelMap
+	value      float64
+	metricType dto.MetricType
+}
+
+func readMetric(m prometheus.Metric) MetricResult {
+	pb := &dto.Metric{}
+	m.Write(pb)
+	labels := make(labelMap, len(pb.Label))
+	for _, v := range pb.Label {
+		labels[v.GetName()] = v.GetValue()
+	}
+	if pb.Gauge != nil {
+		return MetricResult{labels: labels, value: pb.GetGauge().GetValue(), metricType: dto.MetricType_GAUGE}
+	}
+	if pb.Counter != nil {
+		return MetricResult{labels: labels, value: pb.GetCounter().GetValue(), metricType: dto.MetricType_COUNTER}
+	}
+	if pb.Untyped != nil {
+		return MetricResult{labels: labels, value: pb.GetUntyped().GetValue(), metricType: dto.MetricType_UNTYPED}
+	}
+	panic("Unsupported metric type")
+}
+
+func TestQueryShowList(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error opening a stub db connection: %s", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"key", "value"}).
+		AddRow("dns_queries", -1).
+		AddRow("databases", 1).
+		AddRow("pools", 0).
+		AddRow("users", 2)
+
+	mock.ExpectQuery("SHOW LISTS;").WillReturnRows(rows)
+	logger := &slog.Logger{}
+
+	ch := make(chan prometheus.Metric)
+	go func() {
+		defer close(ch)
+		if err := queryShowLists(ch, db, logger); err != nil {
+			t.Errorf("Error running queryShowList: %s", err)
+		}
+	}()
+
+	expected := []MetricResult{
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: -1},
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: 1},
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: 0},
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: 2},
+	}
+
+	convey.Convey("Metrics comparison", t, func() {
+		for _, expect := range expected {
+			m := readMetric(<-ch)
+			convey.So(expect, convey.ShouldResemble, m)
+		}
+	})
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled exceptions: %s", err)
+	}
+}
+
+func TestQueryShowConfig(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error opening a stub db connection: %s", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"key", "value", "default", "changeable"}).
+		AddRow("max_client_conn", 1900, 100, true).
+		AddRow("max_user_connections", 100, 100, true).
+		AddRow("auth_type", "md5", "md5", true).
+		AddRow("client_tls_ciphers", "default", "default", "yes")
+
+	mock.ExpectQuery("SHOW CONFIG;").WillReturnRows(rows)
+	logger := &slog.Logger{}
+
+	ch := make(chan prometheus.Metric)
+	go func() {
+		defer close(ch)
+		if err := queryShowConfig(ch, db, logger); err != nil {
+			t.Errorf("Error running queryShowConfig: %s", err)
+		}
+	}()
+
+	expected := []MetricResult{
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: 1900},
+		{labels: labelMap{}, metricType: dto.MetricType_GAUGE, value: 100},
+	}
+	convey.Convey("Metrics comparison", t, func() {
+		for _, expect := range expected {
+			m := readMetric(<-ch)
+			convey.So(expect, convey.ShouldResemble, m)
+		}
+	})
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled exceptions: %s", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/prometheus-community/pgbouncer_exporter
 go 1.22
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/alecthomas/kingpin/v2 v2.4.0
 	github.com/lib/pq v1.10.9
 	github.com/prometheus/client_golang v1.20.4
 	github.com/prometheus/common v0.60.0
 	github.com/prometheus/exporter-toolkit v0.13.0
+	github.com/smartystreets/goconvey v1.8.1
 )
 
 require (
@@ -15,7 +17,9 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/mdlayher/vsock v1.2.1 // indirect
@@ -23,6 +27,7 @@ require (
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/smarty/assertions v1.15.0 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
